ec68d7690beb06fc674c8cfa750c44c59fb68db4
Incrisz <incrisz@localhost.localdomain>
2023-05-19 00:54:59 -0400

    easelow

--- easelow-setup/install.sh
+++ easelow-setup/install.sh
@@ -21,7 +21,7 @@ sudo apt install php8.0-dom
 sudo apt install php8.0-gd -y
 sudo apt install php8.0-intl  
 sudo apt install php8.0-mbstring -y
-sudo apt install php8.0-simplexml
+# sudo apt install php8.0-simplexml
 sudo apt install php8.0-xml 
 sudo apt install php8.0-xsl 
 sudo apt install php8.0-zip -y
@@ -33,17 +33,26 @@ sudo apt-get install php8.0-curl
 curl -sS https://getcomposer.org/installer | sudo php8.0 -- --install-dir=/usr/local/bin --filename=composer
 
 
-
 # Folder for git work
+
+sudo git clone https://github.com/Incrisz/afriada.git
+cd afriada
+cd app/
 sudo git clone https://github.com/Incrisz/exceptions.git
-sudo cd app/
 sudo mv exceptions/Exceptions /var/www/html/afriada/app/
-  
-sudo rm -r exceptions
+cd ..
 sudo mkdir storage/framework/cache/data
 
-find . -type f -exec chmod 644 {} \;
-find . -type d -exec chmod 755 {} \;
+# composer tester
+
+sudo composer update
+cd app/
+sudo mv exceptions/CoreComponentRepository.php /var/www/html/afriada/vendor/mehedi-iitdu/core-component-repository/src/
+sudo rm -r exceptions
+cd ..
+
+sudo find . -type f -exec chmod 644 {} \;
+sudo find . -type d -exec chmod 755 {} \;
 
 sudo chown -R www-data:www-data .
 sudo chmod -R 755 .
@@ -51,7 +60,7 @@ sudo chmod -R 755 storage
 sudo chmod -R 755 bootstrap/cache
 
 # To test the whole setup
-php artisan cache:clear
-php artisan config:clear
-php artisan config:cache
-php artisan optimize:clear
+sudo php artisan cache:clear
+sudo php artisan config:clear
+sudo php artisan config:cache
+sudo php artisan optimize:clear
