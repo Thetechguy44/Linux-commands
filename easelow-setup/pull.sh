00ad0c5c48c55994d8cc1cdb720c34c050ee0a8d
Incrisz <incrisz@localhost.localdomain>
2023-05-20 00:47:59 -0400

    updatesd

---
+++ easelow-setup/pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/install.sh 
+sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/update_php_ini_file.sh 
+
+sudo chmod +x install.sh
+sudo ./install.sh
+
+sudo chmod +x update_php_ini_file.sh 
+sudo ./update_php_ini_file.sh 
