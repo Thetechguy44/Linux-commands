5f02d885f9b3dadd35382dc51f131dbb28fd6556
Incrisz <incrisz@localhost.localdomain>
2023-05-20 12:06:21 -0400

    configo

--- easelow-setup/pull.sh
+++ easelow-setup/pull.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/install.sh 
-sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/update_php_ini_file.sh 
 
-
+# installation file
+sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/install.sh
 sudo chmod +x install.sh
 sudo ./install.sh
 
+# php config updated
+sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/update_php_ini_file.sh 
 sudo chmod +x update_php_ini_file.sh 
 sudo ./update_php_ini_file.sh 
+
+# Apache2 site-available config
+sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/config.sh 
+sudo chmod +x config.sh 
+sudo ./config.sh
+
+
+
 
 
 # sudo wget https://github.com/Incrisz/Linux-commands/raw/main/easelow-setup/pull.sh 
