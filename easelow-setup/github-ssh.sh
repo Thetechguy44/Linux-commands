5b2f7492ca05b4f69b7c87a8f9199f8365bb0fba
Incrisz <incrisz@localhost.localdomain>
2023-05-25 03:03:53 -0400

    ssh

--- easelow-setup/github-ssh.sh
+++ easelow-setup/github-ssh.sh
@@ -4,7 +4,8 @@
 sudo wget https://github.com/Incrisz/Linux-commands/raw/main/ssh/id_ed25519
 sudo chmod 600 id_ed25519
 sudo mv id_ed25519 /root/.ssh/id_ed25519
-sudo ssh -T git@github.com -y
+sudo ssh -o StrictHostKeyChecking=no -T git@github.com
+
 
 
 # Generate SSH Key Pair:
