de4ebc149327a851da7bfd950927c9eae539f8e2
Incrisz <incrisz@localhost.localdomain>
2023-05-29 15:35:17 -0400

    update

--- setup/pull.sh
+++ setup/pull.sh
@@ -2,22 +2,22 @@
 
 
 # github-ssh file
-sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/blob/main/setup/github-ssh.sh
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/github-ssh.sh
 sudo chmod +x github-ssh.sh
 sudo ./github-ssh.sh
 
 # installation file
-sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/blob/main/setup/install.sh
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/install.sh
 sudo chmod +x install.sh
 sudo ./install.sh
 
 # php config updated
-sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/blob/main/setup/update_php_ini_file.sh
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/update_php_ini_file.sh
 sudo chmod +x update_php_ini_file.sh 
 sudo ./update_php_ini_file.sh 
 
 # Apache2 site-available config
-sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/blob/main/setup/config.sh
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/config.sh
 sudo chmod +x config.sh 
 sudo ./config.sh
 
@@ -26,7 +26,6 @@ sudo ./config.sh
 
 #!/bin/bash
 
-# sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/blob/main/setup/pull.sh
-
+# sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/pull.sh
 # sudo chmod +x pull.sh
 # sudo ./pull.sh
