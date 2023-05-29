61523741c6d894198ccc3c1238c21d038ff4ed5d
Incrisz <incrisz@localhost.localdomain>
2023-05-29 00:45:53 -0400

    update

--- setup/pull.sh
+++ setup/pull.sh
@@ -2,22 +2,22 @@
 
 
 # github-ssh file
-sudo wget https://github.com/Incrisz/Linux-commands/raw/main/setup/github-ssh.sh
+sudo wget https://github.com/Incrisz/Linux-commands/blob/main/setup/github-ssh.sh
 sudo chmod +x github-ssh.sh
 sudo ./github-ssh.sh
 
 # installation file
-sudo wget https://github.com/Incrisz/Linux-commands/raw/main/setup/install.sh
+sudo wget https://github.com/Incrisz/Linux-commands/blob/main/setup/install.sh
 sudo chmod +x install.sh
 sudo ./install.sh
 
 # php config updated
-sudo wget https://github.com/Incrisz/Linux-commands/raw/main/setup/update_php_ini_file.sh 
+sudo wget https://github.com/Incrisz/Linux-commands/blob/main/setup/update_php_ini_file.sh
 sudo chmod +x update_php_ini_file.sh 
 sudo ./update_php_ini_file.sh 
 
 # Apache2 site-available config
-sudo wget https://github.com/Incrisz/Linux-commands/raw/main/setup/config.sh 
+sudo wget https://github.com/Incrisz/Linux-commands/blob/main/setup/config.sh
 sudo chmod +x config.sh 
 sudo ./config.sh
 
