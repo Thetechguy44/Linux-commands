0e4c9e3f667ab25b1d3a7922fb4c87c2490d333a
Incrisz <incrisz@localhost.localdomain>
2023-06-11 04:54:07 -0400

    prometheus

--- setup/prometheus/grafana.sh
+++ setup/prometheus/grafana.sh
@@ -2,7 +2,10 @@
 
 sudo su
 # Add the Grafana GPG key in Ubuntu using wget
+
 curl https://packages.grafana.com/gpg.key | sudo gpg --dearmor > /usr/share/keyrings/grafana-archive-keyring.gpg
+
+exit
 
 # Now add the Grafana repository in Ubuntu using APT
 echo "deb [signed-by=/usr/share/keyrings/grafana-archive-keyring.gpg] https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
