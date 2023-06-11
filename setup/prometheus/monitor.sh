1b777a99d61eb44f380dee04c6601b34c44e79ea
Incrisz <incrisz@localhost.localdomain>
2023-06-11 00:01:36 -0400

    prometheus

---
+++ setup/prometheus/monitor.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# prometheus file
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/prometheus/prometheus.sh
+sudo chmod +x prometheus.sh
+sudo ./prometheus.sh
+
+# grafana 
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/prometheus/grafana.sh
+sudo chmod +x grafana.sh
+sudo ./grafana.sh
+
+# node_exporter
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/prometheus/node-exporter.sh
+sudo chmod +x node-exporter.sh 
+sudo ./node-exporter.sh 
+
+
+
+#!/bin/bash
+
+# sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/setup/prometheus/monitor.sh
+# sudo chmod +x monitor.sh
+# sudo ./monitor.sh
