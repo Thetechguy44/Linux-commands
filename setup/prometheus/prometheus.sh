dafa8f6748459d145e6ecc417605ff4962579e08
Incrisz <incrisz@localhost.localdomain>
2023-06-11 02:37:13 -0400

    prometheus

--- setup/prometheus/prometheus.sh
+++ setup/prometheus/prometheus.sh
@@ -7,10 +7,10 @@
 sudo apt update -y
 
 # switch to root user
-sudo su -
+# sudo su -
 
 # Export the release of Prometheus
-export RELEASE="2.2.1"
+sudo export RELEASE="2.2.1"
 
 # Using below commands we can create a user and directory.
 sudo useradd --no-create-home --shell /bin/false prometheus  
