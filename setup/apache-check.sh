653258e7062adff5a11252d385b2a0528fe06942
Incrisz <incrisz@localhost.localdomain>
2023-06-14 00:42:37 -0400

    AMI

--- setup/apache-check.sh
+++ setup/apache-check.sh
@@ -2,8 +2,8 @@
 
 # Check Apache status
   if ! pgrep apache2 >/dev/null; then
-    echo "Apache is not running. Restarting..."
+    # echo "Apache is not running. Restarting..."
     sudo service apache2 start
   else
-    echo "Apache is running."
+    # echo "Apache is running."
   fi
