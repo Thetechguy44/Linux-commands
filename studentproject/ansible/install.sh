0974f8d6b480e8c5c17f368620c6a0fa909fc079
Incrisz <incrisz@localhost.localdomain>
2023-07-03 17:26:10 -0400

    ansible

---
+++ studentproject/ansible/install.sh
@@ -0,0 +1,15 @@
+
+sudo apt update
+sudo apt install ansible
+
+sudo apt install python3-pip
+pip3 install ansible -y
+
+sudo apt install ansible-core -y
+sudo apt install ansible -y
+ansible-galaxy collection install amazon.aws
+pip3 install boto3
+
+sudo mkdir /etc/ansible
+sudo touch ansible.cfg
+sudo touch hosts
