dc6591ea409897ac1d9d7cbf6ec565f5dd2ccf61
Incrisz <incrisz@localhost.localdomain>
2023-08-11 04:59:00 -0400

    samba

--- studentproject/ansible/install.sh
+++ studentproject/ansible/install.sh
@@ -1,6 +1,10 @@
 
 sudo apt update
-sudo apt install ansible
+
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo apt update
+
+sudo apt install ansible -y
 
 sudo apt install python3-pip
 pip3 install ansible -y
@@ -10,6 +14,31 @@ sudo apt install ansible -y
 ansible-galaxy collection install amazon.aws
 pip3 install boto3
 
-sudo mkdir /etc/ansible
-sudo touch ansible.cfg
-sudo touch hosts
+sudo cd /etc/ansible
+# sudo nano ansible.cfg
+
+ansible_cfg_path="/etc/ansible/ansible.cfg"
+
+# Check if the file exists
+if [ -f "$ansible_cfg_path" ]; then
+  # Append the lines to the file
+  echo "[inventory]" >> "$ansible_cfg_path"
+  echo "enable_plugins = aws.ec2" >> "$ansible_cfg_path"
+  
+fi
+
+
+sudo cd /opt
+sudo touch aws_ec2.yaml
+yaml="/opt/aws_ec2.yaml"
+
+# Check if the file exists
+if [ -f "$yaml" ]; then
+  # Append the lines to the file
+  echo "plugin: aws_ec2" >> "$yaml"
+  echo "regions:
+         - af-south-1" >> "$yaml"
+  
+fi
+
+sudo ansible-inventory -i /opt/aws_ec2.yaml
