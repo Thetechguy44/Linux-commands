334124c1c8b035d1e73a15281b6602d1155a30c4
incrisz <incrisz4luv@gmail.com>
2023-10-28 11:10:20 +0100

    cmd

---
+++ cmds/cmd.sh
@@ -0,0 +1,8 @@
+# Be careful when using this command, as it is irreversible.
+
+# Delete all files and folders in the current directory
+rm -rf *
+
+# If you want to keep the directory itself, but delete all of its contents, you can use the following command instead:
+
+rm -rf ./*
