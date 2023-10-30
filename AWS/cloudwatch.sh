74a3853cfea864c546852b0b0e3f05ef4c6bb513
incrisz <incrisz4luv@gmail.com>
2023-10-30 08:48:07 +0100

    cloudwatch

---
+++ AWS/cloudwatch.sh
@@ -0,0 +1,15 @@
+# create IAM ROLE
+# Permissions policies 
+-CloudWatchAgentAdminPolicy
+
+
+
+# Steps to install and configure cloudwatch agent 
+
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+sudo apt-get update -y
+sudo apt-get install collectd
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status
