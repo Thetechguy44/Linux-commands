b6da964bac7d7843a7d285cd83659e2616ec2dfc
incrisz <incrisz4luv@gmail.com>
2023-11-01 06:42:38 +0100

    cloudwatch

--- AWS/cloudwatch.sh
+++ AWS/cloudwatch.sh
@@ -9,7 +9,18 @@
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+# apache log dir
+ /var/log/apache2/access.log
+
+# Do you want to specify any additional log files to monitor?
+ /var/log/apache2/error.log
+
+# Do you want to specify any additional log files to monitor?
+/var/www/html/update/storage/logs/laravel.log
+# To check configuration file 
+cat /opt/aws/amazon-cloudwatch-agent/bin/config.json
+
 sudo apt-get update -y
-sudo apt-get install collectd
+sudo apt-get install collectd -y
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a status
