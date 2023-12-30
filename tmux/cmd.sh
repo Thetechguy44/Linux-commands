c666a653e1f7c39d8b7d1d005ee991372596335b
Incrisz <incrisz@localhost.localdomain>
2023-12-30 16:29:12 -0500

    nc

--- tmux/cmd.sh
+++ tmux/cmd.sh
@@ -11,3 +11,9 @@ ctrl + B then D
 
 ctrl + B then type
 :kill-session
+
+
+As with most environments, tmux offers kill-* commands:
+Ctrl+B :kill-session – kill the active session, all its windows and panes.
+Ctrl+B :kill-window or Ctrl+B & – kill the active window and all panes within it.
+Ctrl+B :kill-pane or Ctrl+B x – kill the active pane.
