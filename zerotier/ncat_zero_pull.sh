fdb079793e17fd1603193317726a1e14d2ff7804
Incrisz <incrisz@localhost.localdomain>
2023-12-31 06:12:15 -0500

    ncat and zerotier automated/setup script

---
+++ zerotier/ncat_zero_pull.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install on linux
+sudo curl -s https://install.zerotier.com | sudo bash
+
+# install netcat on linux
+sudo apt-get -y install ncat
+
+# check service
+systemctl start zerotier-one
+
+# join a network on winows / linux
+zerotier-cli join abfd31bd47502384
+
+# pull big-guy
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/netcat/script.sh
+sudo chmod +x script.sh
+sudo ./script.sh
