38915252507d6336db8850976d6a062dad7b4713
Incrisz <incrisz@localhost.localdomain>
2023-12-31 06:00:35 -0500

    update

--- netcat/script.sh
+++ netcat/script.sh
@@ -2,26 +2,26 @@
 
 cd /var
 
-# sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/netcat/systemd.service
-# sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/netcat/auto-run.sh
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/netcat/baby.service
+sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/netcat/auto-run.sh
 
 sudo chmod +x auto-run.sh
-# sudo chmod +x systemd.service
+sudo chmod +x baby.service
 
 # this will start our file both on reboot and start-up
 
 # Create a systemd service unit:
 
 # Create a file, for example, /etc/systemd/system/restart_listener.service, and add the following content:
-sudo mv restart_listener.service /etc/systemd/system/restart_listener.service
+sudo mv baby.service /etc/systemd/system/baby.service
 
-
-sudo nohup ./auto-run.sh > /dev/null 2>&1 &
+# to always restart script
+# sudo nohup ./auto-run.sh > /dev/null 2>&1 &
 
 # Replace /home/ubuntu/script.sh with the actual path to your script.
 
 # Enable and start the service:
-sudo systemctl enable restart_listener.service
-sudo systemctl start restart_listener.service
+sudo systemctl enable baby.service
+sudo systemctl start baby.service
 
 # sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/main/netcat/script.sh
