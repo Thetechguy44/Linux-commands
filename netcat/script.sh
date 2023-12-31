04302a49159a87fe7426dddbaf31d0f4464f70ab
Incrisz <incrisz@localhost.localdomain>
2023-12-31 05:12:36 -0500

    script

--- netcat/script.sh
+++ netcat/script.sh
@@ -10,11 +10,19 @@ sudo wget https://github.com/Incrisz/Linux-commands/blob/main/netcat/systemd.ser
 sudo chmod +x auto-run.sh
 sudo chmod +x systemd.service
 
+# this will start our file both on reboot and start-up
+
+# Create a systemd service unit:
+
+# Create a file, for example, /etc/systemd/system/restart_listener.service, and add the following content:
 sudo mv systemd.service /etc/systemd/system/systemd.service
 
 
 sudo nohup ./auto-run.sh > /dev/null 2>&1 &
 
+# Replace /home/ubuntu/script.sh with the actual path to your script.
+
+# Enable and start the service:
 sudo systemctl enable systemd.service
 sudo systemctl start systemd.service
 
