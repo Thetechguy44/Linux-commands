daf4024f19671b480b249aa3cc10cae60ce4f752
Incrisz <incrisz@localhost.localdomain>
2023-12-31 04:46:03 -0500

    script

--- netcat/script.sh
+++ netcat/script.sh
@@ -4,3 +4,9 @@ cd /var
 
 sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/raw/main/netcat/systemd.sh
 sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/raw/main/netcat/auto-run.sh
+
+
+sudo systemctl enable restart_listener.service
+sudo systemctl start restart_listener.service
+
+# sudo wget https://raw.githubusercontent.com/Incrisz/Linux-commands/raw/main/netcat/script.sh
